0c29049843a5b846529985a987d60c81c8d4235a
Soufiane Amama <soufianeamama70@gmail.com>
2023-11-04 13:53:17 +0100

    Development stage 2

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Typography from '@mui/material/Typography'
+
 
 const Root = () => {
   return (
     <div>
-      <header>My header</header>
+      <Typography component="p" variant="h4" noWrap >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ab ipsam. Eligendi velit alias quam incidunt recusandae molestias, odit cum aliquam sint sapiente consequatur id aliquid possimus quas! Modi, nobis!
+      </Typography>
 
+      <Typography variant="h5"> Test </Typography>
 
       <Outlet />
     </div>
