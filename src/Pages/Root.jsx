6fa4a3eec8a9ad364ecc332255df763d6faec3c3
Soufiane Amama <soufianeamama70@gmail.com>
2023-11-04 15:22:52 +0100

    Development stage 4 - MUI Button & IconButton Components

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,17 +1,29 @@
 import { Outlet } from 'react-router-dom'
 import Typography from '@mui/material/Typography'
+import { Button, IconButton } from '@mui/material'
+import { Delete } from '@mui/icons-material'
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 const Root = () => {
   return (
     <div>
       <Typography 
-        className='title' 
-        sx={{padding: "10px", textAlign: "center"}} 
-        border="4px solid green" mt="20px" // mt={5}
-        color="primary.main"  // dynamic color with light mode - dark mode 
+        color="primary.main"
         variant="h4"
       > Soufiane Amama </Typography>
+
+      <Button 
+        sx={{ mr: "auto", ml: "auto", display: "flex"}}
+        endIcon={<Delete />} 
+        variant='contained' 
+        color='error' >
+        Click here
+      </Button> 
+
+      <IconButton size='large' color='info'>
+        <MenuIcon fontSize='inherit' />
+      </IconButton>
 
       <Outlet />
     </div>
