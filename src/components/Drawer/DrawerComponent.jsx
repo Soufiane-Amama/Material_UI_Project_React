5e47ac324eda88ac379137f5b85a48280ba6ca39
Soufiane Amama <soufianeamama70@gmail.com>
2023-11-25 00:11:57 +0100

    Development stage 7 - MUI List & ListItem Components

--- src/components/Drawer/DrawerComponent.jsx
+++ src/components/Drawer/DrawerComponent.jsx
@@ -1,36 +1,78 @@
-/* eslint-disable no-undef */
-// @ts-nocheck
-import Drawer from '@mui/material/Drawer';
-import Toolbar from '@mui/material/Toolbar';
-// import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemButton from '@mui/material/ListItemButton';
-// import ListItemIcon from '@mui/material/ListItemIcon';
-// import ListItemText from '@mui/material/ListItemText';
-// import InboxIcon from '@mui/icons-material/MoveToInbox';
-// import MailIcon from '@mui/icons-material/Mail';
-
+import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { Home, Create, Person, Settings, Logout } from "@mui/icons-material";
 
 const DrawerComponent = ({ drawerWidth }) => {
   return (
     <Drawer
-        sx={{
+      sx={{
+        width: `${drawerWidth}px`,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
           width: `${drawerWidth}px`,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: `${drawerWidth}px`,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
+          boxSizing: "border-box",
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar />
+      <Divider />
 
-      </Drawer>
-  )
-}
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
 
-export default DrawerComponent
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Create />
+            </ListItemIcon>
+            <ListItemText primary="Create" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Logout />
+            </ListItemIcon>
+            <ListItemText primary="Logout" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
+
+export default DrawerComponent;
